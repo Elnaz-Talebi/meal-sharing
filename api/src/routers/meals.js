9bbdafb82de48a4380adb932cb19faca4545a3ce
Elnaz-Talebi <nel.nazi1987@gmail.com>
2025-05-29 00:27:17 +0200

    apply comments

--- api/src/routers/meals.js
+++ api/src/routers/meals.js
@@ -4,46 +4,84 @@ import StatusCodes from "http-status-codes";
 const mealsRouter = express.Router();
 
 mealsRouter.get("/", async (req, res) => {
-  const meals = await knex("meal").select("*");
-  res.json(meals);
+  try {
+    const meals = await knex("meal").select("*");
+    res.json(meals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 mealsRouter.post("/", async (req, res) => {
-  const data = req.body;
-  const result = await knex("meal").insert(data);
-  res.status(StatusCodes.CREATED).json(result);
+  try {
+    const data = req.body;
+    const result = await knex("meal").insert(data);
+    res.status(StatusCodes.CREATED).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 mealsRouter.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const [meal] = await knex("meal").select().where("id", id);
-  if (!meal) {
-    res.status(StatusCodes.NOT_FOUND).json({ message: "Meal is not exist." });
-  } else {
-    res.json(meal);
+  try {
+    const id = req.params.id;
+    const [meal] = await knex("meal").select().where("id", id);
+    if (!meal) {
+      res.status(StatusCodes.NOT_FOUND).json({ message: "Meal is not exist." });
+    } else {
+      res.json(meal);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 mealsRouter.put("/:id", async (req, res) => {
-  const data = req.body;
-  const id = req.params.id;
-  const [meal] = await knex("meal").select().where("id", id);
-  if (!meal) {
-    res.status(StatusCodes.NOT_FOUND).json({ message: "Meal is not exist." });
-  } else {
-    await knex("meal").where("id", id).update(data);
-    res.status(StatusCodes.OK).json({ message: "Meal update successfully." });
+  try {
+    const data = req.body;
+    const id = parseInt(req.params.id);
+    if (id === NaN) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Id must be integer." });
+    }
+
+    const result = await knex("meal").where("id", id).update(data);
+    result > 0
+      ? res
+          .status(StatusCodes.OK)
+          .json({ message: "Meal update successfully." })
+      : res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "Meal is not exist." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
 mealsRouter.delete("/:id", async (req, res) => {
-  const id = req.params.id;
-  const [meal] = await knex("meal").select().where("id", id);
-  if (!meal) {
-    res.status(StatusCodes.NOT_FOUND).json({ message: "Meal is not exist." });
-  } else {
-    await knex("meal").where("id", id).delete();
-    res.status(StatusCodes.OK).json({ message: "Meal deleted successfully." });
+  try {
+    const id = parseInt(req.params.id);
+    if (id === NaN) {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Id must be integer." });
+    }
+    const result = await knex("meal").where("id", id).delete();
+    result > 0
+      ? res
+          .status(StatusCodes.OK)
+          .json({ message: "Meal deleted successfully." })
+      : res
+          .status(StatusCodes.NOT_FOUND)
+          .json({ message: "Meal is not exist." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
